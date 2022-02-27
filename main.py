35d8a641146b9a0b426d79327f99b85942e30af7
Aseer-Ahmad <37152612+Aseer-Ahmad@users.noreply.github.com>
2022-02-27 23:48:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import argparse
 import math
 
 from dataloader import get_cifar10, get_cifar100
-from utils      import accuracy
+from utils import accuracy
 
-from model.wrn  import WideResNet
+from model.wrn import WideResNet
 
 import torch
 import torch.optim as optim
-from torch.utils.data   import DataLoader
+from torch.utils.data import DataLoader
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.optim import lr_scheduler
@@ -16,4 +16,3 @@ from torch.optim import lr_scheduler
 import sys
 
 import logging
-logging.basicConfig(filename="log/debug.log", filemode='a',format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s', datefmt='%H:%M:%S', level=logging.DEBUG)
