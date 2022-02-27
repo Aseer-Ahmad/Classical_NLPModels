19f1fc3661a8c12b5a1618e3b5280711f9789d51
Aseer-Ahmad <37152612+Aseer-Ahmad@users.noreply.github.com>
2022-02-27 23:40:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,11 @@ from model.wrn  import WideResNet
 import torch
 import torch.optim as optim
 from torch.utils.data   import DataLoader
-
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.optim import lr_scheduler
+
 import sys
 
 import logging
 logging.basicConfig(filename="log/debug.log", filemode='a',format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s', datefmt='%H:%M:%S', level=logging.DEBUG)
-
